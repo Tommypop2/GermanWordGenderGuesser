2aae5990da2e312440e6eb0657cd54682091ba6e
BuildTools <71586988+Tommypop2@users.noreply.github.com>
2022-08-21 17:09:48 +0100

    Updated predict.py

---
+++ predict.py
@@ -0,0 +1,41 @@
+from operator import index, indexOf
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import generateTrainingData
+import helperFunctions
+model = keras.Sequential([
+    keras.layers.Dense(
+        units=72, activation="relu"),
+    keras.layers.Dense(units=144, activation='relu'),
+    keras.layers.Dense(units=288, activation='relu'),
+    keras.layers.Dense(units=1152, activation='relu'),
+    keras.layers.Dense(units=288, activation="relu"),
+    keras.layers.Dense(units=144, activation="relu"),
+    keras.layers.Dense(units=72, activation="relu"),
+    keras.layers.Dense(units=36, activation="relu"),
+    keras.layers.Dense(units=18, activation="relu"),
+    keras.layers.Dense(units=9, activation="relu"),
+    keras.layers.Dense(units=3, activation="softmax")
+])
+
+
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.load_weights("./weights")
+model.build(input_shape=(None, 72))
+
+print(model.summary())
+while True:
+    inp = input("Enter a word: ").lower()
+    if (inp == "exit"):
+        break
+    predictionArr = model.predict([helperFunctions.convertStrToArr(inp)])
+    genderVal = indexOf(predictionArr[0], max(predictionArr[0]))
+    genders = {0: "masculine", 1: "feminine", 2: "neuter"}
+    print(genders[genderVal])
+    # print(predictionArr[0])
+# print(model.predict(
+#     [helperFunctions.convertStrToArr("vater"), helperFunctions.convertStrToArr("gesundheit")]))
+# print(model.predict([[ord(i) for i in "-" * 68],[ord(i) for i in "-" * 68]]))
+# model.summary()
