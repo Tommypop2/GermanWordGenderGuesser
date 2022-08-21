15bfa3c7d5c3cd0550fc28d34b4769fbdd764ebc
BuildTools <71586988+Tommypop2@users.noreply.github.com>
2022-08-21 21:28:35 +0100

    - Now shows probability for each outcome

--- predict.py
+++ predict.py
@@ -28,10 +28,27 @@ while True:
     if (inp == "exit"):
         break
     predictionArr = model.predict([helperFunctions.convertStrToArr(inp)])
-    genderVal = indexOf(predictionArr[0], max(predictionArr[0]))
-    genders = {0: "masculine", 1: "feminine", 2: "neuter"}
-    print(genders[genderVal])
-    # print(predictionArr[0])
+    scaler = 100/sum(predictionArr[0])
+    percentages = [{"masculine": round(predictionArr[0][0] * scaler, 2)},
+                   {"feminine": round(predictionArr[0][1] * scaler, 2)},
+                   {"neuter": round(predictionArr[0][2] * scaler, 2)}]
+    while True:
+        swaps = 0
+        for i in range(1, len(percentages)):
+            currentElement = percentages[i]
+            previousElement = percentages[i-1]
+            if (currentElement[list(percentages[i].keys())[0]] > previousElement[list(percentages[i-1].keys())[0]]):
+                percentages[i] = previousElement
+                percentages[i-1] = currentElement
+                swaps += 1
+        if (swaps == 0):
+            break
+    for i in percentages:
+        key = list(i.keys())[0]
+        value = i[key]
+        print(f"{value}% chance {key}")
+    print("\n\n")
+# print(predictionArr[0])
 # print(model.predict(
 #     [helperFunctions.convertStrToArr("vater"), helperFunctions.convertStrToArr("gesundheit")]))
 # print(model.predict([[ord(i) for i in "-" * 68],[ord(i) for i in "-" * 68]]))
