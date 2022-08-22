84473a2b9f2f67b5de99541df24e6e81e11f7556
BuildTools <71586988+Tommypop2@users.noreply.github.com>
2022-08-22 08:53:42 +0100

    - Strips leading and trailing whitespace from input strings

--- predict.py
+++ predict.py
@@ -5,6 +5,7 @@ while True:
     inp = input("Enter a word: ").lower()
     if (inp == "exit"):
         break
+    inp = str(inp).strip()
     percentages = predictor.predict(inp)
     for i in percentages:
         key = list(i.keys())[0]
